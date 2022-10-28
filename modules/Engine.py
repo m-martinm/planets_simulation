0fb884479ab8bfcb58cfe7d4b872c4dbf89567dc
Martin <61714317+m-martinm@users.noreply.github.com>
2022-10-28 02:23:59 +0200

    Update Engine.py

--- modules/Engine.py
+++ modules/Engine.py
@@ -1,25 +1,35 @@
+from datetime import tzinfo
 import pygame
 from modules.Constants import *
-from pandas import Timestamp
+from pandas import *
+
 
 class Engine:
 
     planet_list = []
-    
+    pygame.init()
+
     def __init__(self, width, height):
         self.w = width
         self.h = height
         self.size = width, height
         self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont('freeserif', 22)
+
         # global variables and start
-        pygame.init()
+        Engine.fps = 30
         Engine.time = Timestamp.today(tz="UTC").to_julian_date()
         Engine.screen = pygame.display.set_mode(self.size)
-        Engine.center = (self.w/2, self.h /2)
+        Engine.center = (self.w/2, self.h / 2)
 
     def render(self):
+        date = self.font.render(
+            f"Date: {to_datetime(Engine.time, origin= 'julian', unit='D').date()}", False, WHITE)
+        dateRect = date.get_rect()
+        dateRect.topleft = (30, 30)
         Engine.screen.fill(BLACK)
-        self.clock.tick(30)
+        Engine.screen.blit(date, dateRect)
+        self.clock.tick(Engine.fps)
         for planet in Engine.planet_list:
             planet.update()
             planet.display()
